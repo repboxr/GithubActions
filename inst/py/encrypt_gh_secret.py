c5c1878606864fd7f84edbbb50fbdb2502f679ef
Sebastian Kranz <sebkranz@gmail.com>
2023-02-21 11:50:21 +0100

    Delete encrypt_gh_secret.py

--- inst/py/encrypt_gh_secret.py
+++
@@ -1,9 +0,0 @@
-from base64 import b64encode
-from nacl import encoding, public
-
-def py_encrypt_gh_secret(public_key: str, secret_value: str) -> str:
-  """Encrypt a Unicode string using the public key."""
-  public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
-  sealed_box = public.SealedBox(public_key)
-  encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
-  return b64encode(encrypted).decode("utf-8")
